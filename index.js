77cd4336b1a182e64e60eb4c44cc7e2aad898ca6
taschdo <taschdo@gmail.com>
2020-01-19 15:08:06 +0300

    15:07 19/01/2020

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
 const http = require('http')
+const path = require('path')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    if(req.method === 'GET') {
-        res.end(`
-            
-        `)
+    if (req.method === 'GET') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=utf-8'
+        })
+
+        if (req.url === '/') {
+            fs.readFile(
+                path.join(__dirname, 'views', 'index.html'),
+                'utf-8',
+                (err, content) => {
+                    if (err) {
+                        throw err
+                    }
+
+                    res.end(content)
+                }
+            )
+        } else if(req.url === '/about') {
+            fs.readFile(
+                path.join(__dirname, 'views', 'about.html'),
+                'utf-8',
+                (err, content) => {
+                    if (err) {
+                        throw err
+                    }
+
+                    res.end(content)
+                }
+            )
+        } else  if(req.url === '/api/users') {
+            res.writeHead(200, {
+                'Content-Type': 'text/json; charset=utf-8'
+            })
+
+            const users = [
+                {name: 'Vladilen', age: 25},
+                {name: 'Taschdo', age: 30}
+            ]
+
+            res.end(JSON.stringify(users))
+        }
     }
 })
 
